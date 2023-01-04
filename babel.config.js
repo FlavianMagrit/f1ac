f3b76fbf49ea75bf31c96a7ef0429ceb91a3f7d3
Flavian Magrit <flavian.magrit@decathlon.com>
2023-01-04 07:40:00 +0100

    feat(dep): add prettier and typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  // ... some other config
+  plugins: [
+    // ... some other plugins
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          /**
+           * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
+           */
+          '^~(.+)': './src/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+  ],
 };
---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export const MenuCard = () => {
+  return (
+    <View>
+      <Text>f[pwokpjinfuibwofniwof</Text>
+    </View>
+  );
+};
