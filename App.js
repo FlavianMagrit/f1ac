1068e0a0dd970fc14724087ddde34ed15e6476c1
Flavian Magrit <flavian.magrit@decathlon.com>
2023-01-04 14:51:13 +0100

    feat(page): driver page with call api

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Homepage } from "./src/pages/Homepage";
+import { View } from "react-native";
 import { DriversPage } from "./src/pages/DriversPage";
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Homepage}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen name="Drivers" component={DriversPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <DriversPage />
+    </View>
   );
-};
-
-export default App;
+}
--- src/pages/DriversPage.tsx
+++ src/pages/DriversPage.tsx
@@ -1,10 +1,56 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { DriversCard } from "../components/DriversCard";
 
 export const DriversPage = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [season, setSeason] = useState(2022);
+
+  const getDrivers = async (year) => {
+    try {
+      const response = await fetch(
+        `https://v1.formula-1.api-sports.io/rankings/drivers?season=${year}`,
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key": "f41fc9a65ad924ca7dc524fc2d38ca68",
+          },
+        }
+      );
+      const json = await response.json();
+      setData(json.response);
+      setSeason(year);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDrivers(2021);
+  }, []);
+
+  console.log(data);
+
   return (
-    <View>
-      <Text>Drivers</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="font-bold text-6xl mt-14 mb-6 mx-4">Drivers</Text>
+      <Text className="font-bold text-6xl mt-14 mb-6 mx-4">Année {season}</Text>
+      <ScrollView className="mt-8">
+        {data.map((driver) => (
+          <DriversCard
+            firstname={driver.driver.name}
+            lastname={driver.driver.name}
+            number={driver.driver.number}
+            pilotImage={driver.driver.image}
+            podium={driver.position}
+            points={driver.points}
+            stable={driver.team.name}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
