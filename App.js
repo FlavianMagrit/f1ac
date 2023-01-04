8acdf8053148ffe7f3da25204176e00213920932
Flavian Magrit <flavian.magrit@decathlon.com>
2023-01-04 13:48:01 +0100

    bug lib navigation

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { DriversCard } from "~/components/DriversCard";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Homepage } from "./src/pages/Homepage";
+import { DriversPage } from "./src/pages/DriversPage";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View className="flex-1 justify-center bg-violet-500">
-      <Text>Open up App.js to start working on your app!</Text>
-      <DriversCard />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Homepage}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Drivers" component={DriversPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
---
+++ src/pages/DriversPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const DriversPage = () => {
+  return (
+    <View>
+      <Text>Drivers</Text>
+    </View>
+  );
+};
