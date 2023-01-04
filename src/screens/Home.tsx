8397c1584c11531d6f0cb37df6a25c06f8502a3f
Flavian Magrit <flavian.magrit@decathlon.com>
2023-01-04 18:08:28 +0100

    feat: homepage

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Button, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, View, Text, Image, ScrollView } from "react-native";
+import { ArticleCard } from "../components/ArticleCard";
 
-export const Home = ({ navigation }) => {
+export const Home = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = async () => {
+    try {
+      const response = await fetch(
+        `https://newsdata.io/api/1/news?apikey=pub_15267b82367744d037844b9535bd615c0ed57&q=f1&language=en`,
+        { method: "GET" }
+      );
+      const json = await response.json();
+      setArticles(json.results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Button
-        title="Navigate to second screen with french"
-        onPress={() => navigation.navigate("Second", { language: "french" })}
+    <SafeAreaView className="h-full m-4 pb-10">
+      <Image
+        className="w-32 h-32 ml-4"
+        source={{
+          uri: "https://lezebre.lu/images/detailed/79/45334-Sticker-Formula-1-nouveau-logo-F1.png",
+        }}
       />
-      <Button
-        title="Navigate to second screen with english"
-        onPress={() => navigation.navigate("Driver", { language: "english" })}
-      />
-      <StatusBar style="auto" />
-    </View>
+      <Text className="font-bold text-6xl mx-4">News</Text>
+      <ScrollView className="mt-8" showsVerticalScrollIndicator={false}>
+        {articles.map((article, index: number) => (
+          <ArticleCard
+            key={index}
+            title={article.title}
+            articleImage={article.image_url}
+            publishDate={article.pubDate}
+            source={article.source}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
