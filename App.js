0797d5d8ca05a123627248a1027610968e7835d8
Simonbrl <simon.bruyelle59@gmail.com>
2023-01-05 23:48:48 +0100

    fix(login/logout) login & logout bug fixed

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import {
-  FontAwesome,
-  MaterialCommunityIcons,
-  MaterialIcons,
-} from "@expo/vector-icons";
+import { FontAwesome, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 import firebase from "firebase/compat";
 
 import { DriversScreen } from "./src/screens/DriversScreen";
-import { MoreContentScreen } from "./src/screens/MoreContentScreen";
+import { ArticlesScreen } from "./src/screens/ArticlesScreen";
 import { ArticleScreen } from "./src/screens/ArticleScreen";
 import { ConstructorsScreen } from "./src/screens/ConstructorsScreen";
 import { ConstructorScreen } from "./src/screens/ConstructorScreen";
@@ -27,27 +23,29 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) setIsLogged(true);
+      else setIsLogged(false);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <View className="flex-row justify-between">
-        <Image
-          className="w-16 h-16 mt-4 ml-6"
-          source={{
-            uri: "https://lezebre.lu/images/detailed/79/45334-Sticker-Formula-1-nouveau-logo-F1.png",
-          }}
-        />
-        <LogoutButton />
-      </View>
+        <View className="flex-row justify-between">
+          <Image className="w-16 h-16 mt-4 ml-6" source={{uri: "https://lezebre.lu/images/detailed/79/45334-Sticker-Formula-1-nouveau-logo-F1.png"}} />
+          {isLogged && <LogoutButton />}
+        </View>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Tabs"
-          component={TabsNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="MoreContentScreen" component={MoreContentScreen} />
+        <Stack.Screen name="Tabs" component={TabsNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="Articles" component={ArticlesScreen} />
         <Stack.Screen name="Constructor" component={ConstructorScreen} />
         <Stack.Screen name="Article" component={ArticleScreen} />
         <Stack.Screen name="Driver" component={DriverScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -55,67 +53,32 @@ export default function App() {
 
 const TabsNavigator = () => {
   const user = firebase.auth().currentUser;
-
   return (
-    <Tab.Navigator
-      screenOptions={{ tabBarShowLabel: false, headerShown: false }}
-    >
+    <Tab.Navigator screenOptions={{ tabBarShowLabel: false, headerShown: false }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons
-              name="garage-open-variant"
-              color="black"
-              size={30}
-            />
-          ),
-        }}
-      />
+        options={{tabBarIcon: () => <MaterialCommunityIcons name="garage-open-variant" color="black" size={30} />}}/>
       <Tab.Screen
         name="Drivers"
         component={DriversScreen}
-        options={{
-          tabBarIcon: () => (
-            <FontAwesome name="drivers-license" color="black" size={30} />
-          ),
-        }}
-      />
+        options={{tabBarIcon: () => <FontAwesome name="drivers-license" color="black" size={30} />}} />
       <Tab.Screen
         name="Constructors"
         component={ConstructorsScreen}
-        options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons
-              name="keyboard-f1"
-              size={48}
-              color="black"
-            />
-          ),
-        }}
+        options={{tabBarIcon: () => <MaterialCommunityIcons name="keyboard-f1" size={48} color="black" />}}
       />
       {
         user ?
-        <Tab.Screen
-          name="MoreContentScreen"
-          component={MoreContentScreen}
-          options={{
-            tabBarIcon: () => (
-              <MaterialIcons name="keyboard-control" color="black" size={48} />
-            ),
-          }}
-        />
+          <Tab.Screen
+            name="Articles"
+            component={ArticlesScreen}
+            options={{tabBarIcon: () => <MaterialIcons name="keyboard-control" color="black" size={48} />}}/>
         :
-        <Tab.Screen
-          name="User"
-          component={LoginScreen}
-          options={{
-            tabBarIcon: () => (
-              <MaterialIcons name="keyboard-control" color="black" size={48} />
-            ),
-          }}
-        />
+          <Tab.Screen
+            name="User"
+            component={LoginScreen}
+            options={{tabBarIcon: () => <MaterialIcons name="keyboard-control" color="black" size={48} />}}/>
       }
     </Tab.Navigator>
   );
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,25 +1,19 @@
 import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { auth } from "../config/firebaseConfig";
-import { useNavigation } from "@react-navigation/core";
-
+import { useNavigation } from "@react-navigation/native";
 export const LogoutButton = () => {
+
   const navigation = useNavigation();
+
   const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.navigate("Login");
-        console.log("Signed out");
-      })
+    auth.signOut()
+      .then(() => navigation.navigate("Home" as never))
       .catch((error) => alert(error.message));
   };
 
   return (
-    <TouchableOpacity
-      onPress={handleSignOut}
-      className="w-1/4 h-12 mr-4 mb-5 mt-10 rounded-lg bg-black justify-center"
-    >
+    <TouchableOpacity onPress={handleSignOut} className="w-1/4 h-12 mr-4 mb-5 mt-10 rounded-lg bg-black justify-center">
       <Text className="text-lg text-center text-white">Logout</Text>
     </TouchableOpacity>
   );
